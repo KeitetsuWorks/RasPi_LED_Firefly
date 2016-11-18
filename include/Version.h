dcc24529f3d125317d1da18ccc062f6391e8756e
KeitetsuWorks <keitetsu.jp@outlook.com>
2016-11-19 01:20:55 +0900

    定数の重複定義回避策を追加

--- include/Version.h
+++ include/Version.h
@@ -9,5 +9,7 @@
 
 #pragma once    // __VERSION_H__
 
-//#define VERSION           "1.00"          /*!< バージョン情報はGitのタグで管理 */
+#ifndef VERSION
+#define VERSION             "1.00"          /*!< バージョン情報はGitのタグで管理 */
+#endif
 
